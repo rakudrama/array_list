88a44e1c69bef0a02be6c145bcaf093ac897a2ac
Stephen Adams <sra@google.com>
2017-09-25 16:53:56 -0700

    Update travis.sh

--- tool/travis.sh
+++ tool/travis.sh
@@ -9,8 +9,8 @@ set -e
 
 # Verify that the libraries are error and warning-free.
 echo "Running dartanalyzer..."
-dartanalyzer lib/fixnum.dart test/all_tests.dart
+dartanalyzer lib/array_list.dart
 
 # Run the tests.
 echo "Running tests..."
-dart -c test/all_tests.dart
+#dart -c test/all_tests.dart
